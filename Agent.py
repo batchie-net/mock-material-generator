1d10c7774c6569ca89ce200f0b52c9243f839973
cfossto <70951673+cfossto@users.noreply.github.com>
2025-02-25 21:00:38 +0100

    Agent produce or consume

---
+++ Agent.py
@@ -0,0 +1,16 @@
+from materials import Material
+from random import randrange
+
+class Agent:
+
+    def consume(self, material: Material):
+        amount = randrange(50, 15_000)
+        if material.total_quantity > amount:
+            material.total_quantity -= amount
+            return material
+
+    def produce(self, material: Material):
+        amount = randrange(500, 30_000)
+        if material.total_quantity > 0:
+            material.total_quantity += amount
+            return material
